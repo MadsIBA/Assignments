cdab9d8f58e49637d266297afad519524fb1ea9c
Mads <Mads2097@gmail.com>
2019-09-13 13:48:47 +0200

    Assignment JS.Loops.0 - Ver 1.0

--- Javascript/Assignment JS.Loops.1/myFuncLib.js
+++ Javascript/Assignment JS.Loops.1/myFuncLib.js
@@ -1,1 +1,14 @@
-//Text herewetwrwer
+//User propmt
+var num = Number(prompt("Insert a number you want to find the Fibonacci of"));
+
+
+//Recursion
+var fact = function(n) {
+  if(n === 0 || n === 1) {
+    return 1
+  } else {
+    return fact(n - 1) + fact(n - 2);
+  }
+}
+
+console.log(fact(num));
