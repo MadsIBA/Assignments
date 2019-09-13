c7c508bf28d3ad127adf1d3287496240cb979763
Mads <Mads2097@gmail.com>
2019-09-13 08:41:28 +0200

    Added Solution

--- Javascript/Assignment JS.Arrays.2/jsArrays2.js
+++ Javascript/Assignment JS.Arrays.2/jsArrays2.js
@@ -16,3 +16,32 @@ let palindromeArray = (arr, start = 0, end = arr.length - 1) => {
 
 console.log(palindromeArray([1,2,3,2,1]));
 console.log(palindromeArray([1,2,3,2,2]));
+
+
+// - Teacher Solution
+/*
+'use strict';
+
+const first = function (txt) {
+    return txt.charAt(0);
+}
+
+const last = function (txt) {
+    return txt.charAt(txt.length - 1);
+}
+
+const middle = function (txt) {
+    return txt.substring(1, txt.length - 1);
+}
+
+const isPalindrome = function(txt, debug) {
+    if (txt.length <= 1)
+        return true;
+    if (debug)
+        console.log(`${first(txt)} - ${middle(txt)} - ${last(txt)}`);
+    return first(txt) === last(txt) && isPalindrome(middle(txt), debug);
+}
+
+var phrase = prompt('Enter Phrase');
+console.log(`Is ${phrase} a palidrome? ${isPalindrome(phrase, true)}`);
+*/
