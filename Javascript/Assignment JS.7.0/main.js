2bf4ea9917fda316ff4ecfe2422f8aa5541d68a5
mads <mads2097@gmail.com>
2019-09-17 16:09:25 +0200

    Version 1.0 JS.DOM.0 - Assignment JS.7.0

--- Javascript/Assignment JS.7.0/main.js
+++ Javascript/Assignment JS.7.0/main.js
@@ -10,7 +10,51 @@ let empty = function (target) {
     }
 }
 
+let fillLeft = function (target) {
+    let art = target;
+
+    let img = document.createElement('img'); // create element
+    img.setAttribute('src', 'book-icon.png');
+    img.setAttribute('alt', 'book icon');
+    img.setAttribute('width', '64');
+
+    let h1 = document.createElement('h1'); // create element
+    let txt = document.createTextNode('Always'); // create text
+    h1.appendChild(txt); // put on tree
+
+    let par = document.createElement('p'); // create element
+    txt = document.createTextNode("You could have four columns here but you won't. You'll have three like everyone else."); // create text
+    par.appendChild(txt); // put onto tree
+
+    art.appendChild(img);
+    art.appendChild(h1);
+    art.appendChild(par);
+}
+
 let fillCenter = function (target) {
+    let art = target;
+
+    let img = document.createElement('img'); // create element
+    img.setAttribute('src', 'book-icon.png');
+    img.setAttribute('alt', 'book icon');
+    img.setAttribute('width', '64');
+
+    let h1 = document.createElement('h1'); // create element
+    let txt = document.createTextNode('Always'); // create text
+    h1.setAttribute('style', 'color: yellow');
+    h1.appendChild(txt); // put on tree
+
+    let par = document.createElement('p'); // create element
+    txt = document.createTextNode("You could have four columns here but you won't. You'll have three like everyone else."); // create text
+    par.appendChild(txt); // put onto tree
+    par.setAttribute('style', 'color: yellow');
+
+    art.appendChild(img);
+    art.appendChild(h1);
+    art.appendChild(par);
+}
+
+let fillRight = function (target) {
     let art = target;
 
     let img = document.createElement('img'); // create element
@@ -40,16 +84,30 @@ let t1 = function (ev) {
         empty(elm);
     } else {
         switch (id) {
+            case 'left': fillLeft(elm); break;
             case 'center': fillCenter(elm); break;
+            case 'right': fillRight(elm); break;
         }
     }
 }
 
-let initialize = function () {
+let initialize1 = function () {
+    let left = $('left');
+    fillLeft(left);
+    left.addEventListener('click', t1, false);
+}
+let initialize2 = function () {
     let center = $('center');
     fillCenter(center);
     center.addEventListener('click', t1, false);
 }
-window.addEventListener('load', initialize);
+let initialize3 = function () {
+    let right = $('right');
+    fillRight(right);
+    right.addEventListener('click', t1, false);
+}
+window.addEventListener('load', initialize1);
+window.addEventListener('load', initialize2);
+window.addEventListener('load', initialize3);
 
 // New Code ---------------------
